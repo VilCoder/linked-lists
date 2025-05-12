7d7888f3f3a9181b06b38b8fc4eca4dcd4f4deda
VilCoder <176792549+VilCoder@users.noreply.github.com>
2025-05-12 15:08:45 -0500

    Feat: Add method pop, contains, insertAt, etc

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,24 @@
 import LinkedList from "./linkedList.js";
 
 const linkedList = new LinkedList();
-linkedList.append(2);
-linkedList.prepend(1);
-linkedList.append(5);
+linkedList.append("dog");
+linkedList.append("cat");
+linkedList.append("parrot");
+linkedList.append("hamster");
+linkedList.append("snake");
+linkedList.append("turtle");
 
 console.log(linkedList.size());
-console.log(linkedList.toString());
 console.log(linkedList.getHead());
 console.log(linkedList.getTail());
-console.log();
-console.log(linkedList.at(4));
+console.log(linkedList.contains('hamster'));
+console.log(linkedList.at(3));
+console.log(linkedList.find('snake'));
+
+linkedList.pop();
+
+linkedList.insertAt(3, 2);
+
+console.log(linkedList.removeAt(4));
+
+console.log(linkedList.toString());
--- src/js/linkedList.js
+++ src/js/linkedList.js
@@ -8,13 +8,17 @@ class Node {
 export default class LinkedList {
   constructor() {
     this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
+  // Adds a new node to the end of the list
   append(value) {
     const newNode = new Node(value);
 
     if (!this.head) {
       this.head = newNode;
+      this.length += 1;
     } else {
       let currentNode = this.head;
 
@@ -23,62 +27,184 @@ export default class LinkedList {
       }
 
       currentNode.nextNode = newNode;
+      this.tail = newNode;
+      this.length += 1;
     }
   }
 
+  // Adds a new node to the beginning of the list
   prepend(value) {
     const newNode = new Node(value);
     newNode.nextNode = this.head;
     this.head = newNode;
+    this.length += 1;
   }
 
+  // Returns the number of nodes in the list
   size() {
-    let currentNode = this.head;
-    let totalSize = 0;
-
-    while (currentNode) {
-      totalSize += 1;
-      currentNode = currentNode.nextNode;
-    }
-
-    return totalSize;
+    return this.length;
   }
 
+  // Returns the first node in the list
   getHead() {
-    return this.head ? this.head.value : "";
+    return !this.head ? null : this.head.value;
   }
 
+  // Returns the last node in the list
   getTail() {
-    if (!this.head) {
-      return "";
-    }
-
-    let currentNode = this.head;
-
-    while (currentNode.nextNode) {
-      currentNode = currentNode.nextNode;
-    }
-
-    return currentNode.value;
+    return !this.tail ? null : this.tail.value;
   }
 
+  // Returns the node at the given index
   at(index) {
-    let currentNode = this.head;
-
-    if (index === 1) {
-      return currentNode.value;
+    if (!this.head) {
+      return null;
     }
 
-    let position = 1;
+    if (index === 0) {
+      return this.head.value;
+    }
+
+    let currentNode = this.head;
+    let position = 0;
 
     while (position < index) {
       currentNode = currentNode.nextNode;
+
+      if (currentNode === null) {
+        return null;
+      }
+
       position += 1;
     }
 
     return currentNode.value;
   }
 
+  // Inserts a new node at the given index
+  insertAt(value, index) {
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    }
+
+    const newNode = new Node(value);
+    let currentNode = this.head;
+    let position = 1;
+
+    while (position < index) {
+      if (currentNode.nextNode === null) {
+        currentNode.nextNode = newNode;
+        this.length += 1;
+        return;
+      }
+
+      currentNode = currentNode.nextNode;
+      position += 1;
+    }
+
+    newNode.nextNode = currentNode.nextNode;
+    currentNode.nextNode = newNode;
+    this.length +=1;
+  }
+
+  // Removes the node at the given index
+  removeAt(index) {
+    if (!this.head) {
+      return null;
+    }
+
+    if (index === 0) {
+      const removeNode = this.head;
+      this.head = this.head.nextNode;
+      this.length -= 1;
+
+      return removeNode.value;
+    }
+
+    let currentNode = this.head;
+    let position = 1;
+    
+    while (position < index) {
+      if (currentNode.nextNode === null) {
+        return null;
+      }
+
+      currentNode = currentNode.nextNode;
+      position += 1;
+    }
+
+    const removeNode = currentNode.nextNode;
+
+    currentNode.nextNode = removeNode.nextNode;
+    this.length -=1;
+
+    return removeNode.value;
+  }
+
+  // Removes the last element in the list
+  pop() {
+    if (!this.head) {
+      return null;
+    }
+
+    if (this.length === 1) {
+      const poppedNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length -= 1;
+
+      return poppedNode.value;
+    }
+
+    let currentNode = this.head;
+    let newTail = currentNode;
+
+    while (currentNode.nextNode) {
+      newTail = currentNode;
+      currentNode = currentNode.nextNode;
+    }
+
+    this.tail = newTail;
+    this.tail.nextNode = null;
+    this.length -= 1;
+
+    return currentNode.value;
+  }
+
+  // Return true if the value is in the list, otherwise false
+  contains(value) {
+    let currentNode = this.head;
+
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return true;
+      }
+
+      currentNode = currentNode.nextNode;
+    }
+
+    return false;
+  }
+
+  // Returns the index of the node with the given value, otherwise null
+  find(value) {
+    let currentNode = this.head;
+    let index = 0;
+
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return index;
+      }
+
+      currentNode = currentNode.nextNode;
+      index += 1;
+    }
+
+    return null;
+  }
+
+  // Represents the objects in the list as strings
   toString() {
     let currentNode = this.head;
     let nodes = "";
